dca37b31fcc716b47ae0a3bcca63129cb3f6bee0
Harsh-Wardhan-404 <harshwardhansaindane@gmail.com>
2025-06-29 20:28:30 +0530

    v1

--- frontend/src/components/Typewriter.tsx
+++ frontend/src/components/Typewriter.tsx
@@ -3,26 +3,26 @@ import { TypewriterEffectSmooth } from "./ui/typewriter.effect";
 export function TypewriterEffectSmoothDemo() {
     const words = [
         {
-            text: "Build",
+            text: "Manage",
         },
         {
-            text: "awesome",
+            text: "your",
         },
         {
-            text: "apps",
+            text: "finances",
         },
         {
             text: "with",
         },
         {
-            text: "Aceternity.",
-            className: "text-blue-500 dark:text-blue-500",
+            text: "Penta.",
+            className: "text-[#2CFF05] dark:text-[#2CFF05]",
         },
     ];
     return (
         <div className="flex flex-col items-center justify-center h-[40rem]  ">
-            <p className="text-neutral-600 dark:text-neutral-200 text-sm sm:text-2xl">
-                The road to freedom starts from here
+            <p className="text-neutral-600 dark:text-neutral-200 text-sm sm:text-2xl  ">
+                Financial clarity at your fingertips.
             </p>
             <TypewriterEffectSmooth words={words} />
             <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
